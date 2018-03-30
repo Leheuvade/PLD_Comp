74006498fa300242bc256b2df991e024aff6a0d6
Louise Fonteneau <louise.fonteneau@insa-lyon.fr>
2018-03-30 22:24:40 +0200

    h

--- Source/Core/data/main.cpp
+++ Source/Core/data/main.cpp
@@ -37,7 +37,7 @@ int main(int , const char **) {
   grammaireParser parser(&tokens);
   tree::ParseTree* tree = parser.programme();
 
-  //grammaireImplBaseVisitor visitor;
+  grammaireImplBaseVisitor visitor;
 
   Programme * p = visitor.visit(tree);
 
