19ac069446acba3a288290cae1715f449326521d
Arkmut <xavier.dussert@gmail.com>
2018-04-03 17:36:08 +0200

    oubli des destructeurs de VisitOutput

--- Source/Core/visitor/DebugVisit.cpp
+++ Source/Core/visitor/DebugVisit.cpp
@@ -27,11 +27,17 @@ VisitOutput* DebugVisit::visit(Programme *p)
 	string val = "Programme: \n\n";
 	for (int i = 0; i < p->varGlobales.size(); i++)
 	{
-		val += static_cast<StringOutput*>(p->varGlobales[i]->accept(this))->val;
+		VisitOutput *v = p->varGlobales[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
 	}
 	for (int i = 0; i < p->definitions.size(); i++)
 	{
-		val += static_cast<StringOutput*>(p->definitions[i]->accept(this))->val;
+		VisitOutput *v = p->definitions[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
 	}
 	return new StringOutput(val);
 }
@@ -39,10 +45,19 @@ VisitOutput* DebugVisit::visit(Programme *p)
 VisitOutput* DebugVisit::visit(Definition* p)
 {
 	string val = "Definition* p: \n";
-	val += std::to_string((int)p->type)+"\n";
-	val += static_cast<StringOutput*>(p->name->accept(this))->val;
-	val += static_cast<StringOutput*>(p->params->accept(this))->val;
-	val += static_cast<StringOutput*>(p->bloc->accept(this))->val;
+	val += std::to_string((int)p->type) + "\n";
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	v = p->params->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	v = p->bloc->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
 
 	return new StringOutput(val);
 }
@@ -51,7 +66,10 @@ VisitOutput* DebugVisit::visit(Parametre* p)
 {
 	string val = "Parametre* p: \n";
 	val += std::to_string((int)p->type) + "\n";
-	val += static_cast<StringOutput*>(p->name->accept(this))->val;
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
 	val += "hasBracket: " + (p->hasBrackets) ? "true\n" : "false\n";
 	return new StringOutput(val);
 }
@@ -100,8 +118,8 @@ VisitOutput* DebugVisit::visit(ExprAppel* p)
 
 VisitOutput* DebugVisit::visit(Name* p)
 {
-	string val = "Name* p:"+p->name;
-			//to_string((int)p->symbole)+" \n";
+	string val = "Name* p:" + p->name;
+	//to_string((int)p->symbole)+" \n";
 	return new StringOutput(val);
 }
 
@@ -162,8 +180,11 @@ VisitOutput* DebugVisit::visit(InitDecl* p)
 VisitOutput* DebugVisit::visit(Initialisation* p)
 {
 	string val = "Initialisation* p: \n";
-	val +=std::to_string((int)p->type);
-	val += static_cast<StringOutput*>(p->name->accept(this))->val;
+	val += std::to_string((int)p->type);
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
 
 	return new StringOutput(val);
 }
--- Source/Core/visitor/MappingNameVisit.cpp
+++ Source/Core/visitor/MappingNameVisit.cpp
@@ -2,7 +2,6 @@
 // Created by Louise Fonteneau on 02/04/2018.
 //
 
-#include <Source/Core/data/init_decl/InitialisationVal.h>
 #include "MappingNameVisit.h"
 #include "StringOutput.h"
 
@@ -14,279 +13,342 @@ MappingNameVisit::~MappingNameVisit() {
 
 VisitOutput* MappingNameVisit::visit(Visitable* p)
 {
-    string val = "Visitable* p: \n";
-    return new StringOutput(val);
+	string val = "Visitable* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Programme *p)
 {
-    this->programme =p;
-    string val = "Programme: \n\n";
-    for (int i = 0; i < p->varGlobales.size(); i++)
-    {
-        val += static_cast<StringOutput*>(p->varGlobales[i]->accept(this))->val;
-    }
-    for (int i = 0; i < p->definitions.size(); i++)
-    {
-        val += static_cast<StringOutput*>(p->definitions[i]->accept(this))->val;
-    }
-    return new StringOutput(val);
+	this->programme = p;
+	string val = "Programme: \n\n";
+	for (int i = 0; i < p->varGlobales.size(); i++)
+	{
+		VisitOutput * v = p->varGlobales[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
+	}
+	for (int i = 0; i < p->definitions.size(); i++)
+	{
+		VisitOutput *v = p->definitions[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
+	}
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Definition* p)
 {
-    this->blocCourant = p;
-    string val = "Definition* p: \n";
-    val += "Type:"+std::to_string(static_cast<Type>((int) p->type))+" \n";
-    val += static_cast<StringOutput*>(p->params->accept(this))->val;
-    val += static_cast<StringOutput*>(p->name->accept(this))->val;
-    val += static_cast<StringOutput*>(p->bloc->accept(this))->val;
-    p->name->setSymbol(p);
-    return new StringOutput(val);
+	this->blocCourant = p;
+	string val = "Definition* p: \n";
+	val += "Type:" + std::to_string(static_cast<Type>((int)p->type)) + " \n";
+	VisitOutput *v = p->params->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	v = p->bloc->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	p->name->setSymbol(p);
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Parametre* p)
 {
-    string val = "Parametre* p: \n";
-    val += "Type:"+std::to_string(static_cast<Type>((int) p->type))+" \n";
-    val += static_cast<StringOutput*>(p->name->accept(this))->val;
-    p->name->setSymbol(p);
-    return new StringOutput(val);
+	string val = "Parametre* p: \n";
+	val += "Type:" + std::to_string(static_cast<Type>((int)p->type)) + " \n";
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	p->name->setSymbol(p);
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(ParametreDefinition* p)
 {
-    string val = "ParametreDefinition* p: \n";
-    for (int i = 0; i < p->parameters.size(); i++)
-    {
-        val += static_cast<StringOutput*>(p->parameters[i]->accept(this))->val;
-    }
-    return new StringOutput(val);
+	string val = "ParametreDefinition* p: \n";
+	for (int i = 0; i < p->parameters.size(); i++)
+	{
+		VisitOutput *v = p->parameters[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
+	}
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Affectation* p)
 {
-    string val = "Affectation* p: \n";
-    return new StringOutput(val);
+	string val = "Affectation* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(AffectationBinaire* p)
 {
-    string val = "AffectationBinaire* p: \n";
-    //p->leftValue->name->setSymbol(mapperSymbol.findSymbol(p->leftValue->name->name, programme, blocCourant));
-    val += static_cast<StringOutput*>(p->leftValue->accept(this))->val;
-    val += "Operateur:"+std::to_string(static_cast<OperateurAffectationBinaire>((int) p->op))+" \n";
-    val += static_cast<StringOutput*>(p->expr->accept(this))->val;
-    return new StringOutput(val);
+	string val = "AffectationBinaire* p: \n";
+	//p->leftValue->name->setSymbol(mapperSymbol.findSymbol(p->leftValue->name->name, programme, blocCourant));
+	VisitOutput *v = p->leftValue->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	val += "Operateur:" + std::to_string(static_cast<OperateurAffectationBinaire>((int)p->op)) + " \n";
+	v = p->expr->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(AffectationUnaire* p)
 {
-    string val = "AffectationUnaire* p: \n";
-    return new StringOutput(val);
+	string val = "AffectationUnaire* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Char* p)
 {
-    string val = "Char* p: \n";
-    return new StringOutput(val);
+	string val = "Char* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Expr* p)
 {
-    string val = "Expr* p: \n";
-    return new StringOutput(val);
+	string val = "Expr* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(ExprAppel* p)
 {
-    string val = "ExprAppel* p: \n";
-    mapperSymbol.appelFnctTrue();
-    val += static_cast<StringOutput*>(p->name->accept(this))->val;
-    val += static_cast<StringOutput*>(p->parameters->accept(this))->val;
-    //Appeler méthode pour savoir à quelle definition est associée cet appel
-    return new StringOutput(val);
+	string val = "ExprAppel* p: \n";
+	mapperSymbol.appelFnctTrue();
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	v = p->parameters->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	//Appeler méthode pour savoir à quelle definition est associée cet appel
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Name* p)
 {
-    string val = "Name* p:"+p->name+"\n";
-    p->setSymbol(mapperSymbol.findSymbol(p->name, programme, blocCourant));
-    return new StringOutput(val);
+	string val = "Name* p:" + p->name + "\n";
+	p->setSymbol(mapperSymbol.findSymbol(p->name, programme, blocCourant));
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(NameTab* p)
 {
-    string val = "NameTab* p: \n";
-    return new StringOutput(val);
+	string val = "NameTab* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Operation* p)
 {
-    string val = "Operation* p: \n";
-    return new StringOutput(val);
+	string val = "Operation* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(OperationBinaire* p)
 {
-    string val = "OperationBinaire* p: \n";
-    val += static_cast<StringOutput*>(p->expr1->accept(this))->val;
-    val += "Operateur:"+std::to_string(static_cast<OperateurBinaire>((int) p->op))+" \n";
-    val += static_cast<StringOutput*>(p->expr2->accept(this))->val;
-    return new StringOutput(val);
+	string val = "OperationBinaire* p: \n";
+	VisitOutput *v = p->expr1->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	val += "Operateur:" + std::to_string(static_cast<OperateurBinaire>((int)p->op)) + " \n";
+	v = p->expr2->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(OperationUnaire* p)
 {
-    string val = "OperationUnaire* p: \n";
-    return new StringOutput(val);
+	string val = "OperationUnaire* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(ParametreAppel* p)
 {
-    string val = "ParametreAppel* p: \n";
-    for (int i = 0; i < p->parameters.size(); i++)
-    {
-        val += static_cast<StringOutput*>(p->parameters[i]->accept(this))->val;
-    }
-    return new StringOutput(val);
+	string val = "ParametreAppel* p: \n";
+	for (int i = 0; i < p->parameters.size(); i++)
+	{
+		VisitOutput *v = p->parameters[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
+	}
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Val* p)
 {
-    string val = "Val* p: \n";
-    val += "Val:"+std::to_string(p->val)+" \n";
-    return new StringOutput(val);
+	string val = "Val* p: \n";
+	val += "Val:" + std::to_string(p->val) + " \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Declaration* p)
 {
-    string val = "Declaration* p: \n";
-    val += "Type:"+std::to_string(static_cast<Type>((int) p->type))+" \n";
-    val += static_cast<StringOutput*>(p->name->accept(this))->val;
-    p->name->setSymbol(p);
-    return new StringOutput(val);
+	string val = "Declaration* p: \n";
+	val += "Type:" + std::to_string(static_cast<Type>((int)p->type)) + " \n";
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	p->name->setSymbol(p);
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(DeclarationTab* p)
 {
-    string val = "DeclarationTab* p: \n";
-    return new StringOutput(val);
+	string val = "DeclarationTab* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InitDecl* p)
 {
-    string val = "InitDecl* p: \n";
-    return new StringOutput(val);
+	string val = "InitDecl* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Initialisation* p)
 {
-    string val = "Initialisation* p: \n";
-    val +=std::to_string((int)p->type);
-    val += static_cast<StringOutput*>(p->name->accept(this))->val;
+	string val = "Initialisation* p: \n";
+	val += std::to_string((int)p->type);
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
 
-    return new StringOutput(val);
+
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InitialisationTab* p)
 {
-    string val = "InitialisationTab* p: \n";
-    return new StringOutput(val);
+	string val = "InitialisationTab* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InitialisationVal* p)
 {
-    string val = "InitialisationVal* p: \n";
-    val += static_cast<StringOutput*>(p->value->accept(this))->val;
-    p->name->setSymbol(p);
-    return new StringOutput(val);
+	string val = "InitialisationVal* p: \n";
+	VisitOutput *v = p->value->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	p->name->setSymbol(p);
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Symbole* p)
 {
-    string val = "Symbole* p: \n";
-    return new StringOutput(val);
+	string val = "Symbole* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InstructionBreak* p)
 {
-    string val = "InstructionBreak* p: \n";
-    return new StringOutput(val);
+	string val = "InstructionBreak* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InstructionControle* p)
 {
-    string val = "InstructionControle* p: \n";
-    return new StringOutput(val);
+	string val = "InstructionControle* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InstructionExpr* p)
 {
-    string val = "InstructionExpr* p: \n";
-    val += static_cast<StringOutput*>(p->expr->accept(this))->val;
-    return new StringOutput(val);
+	string val = "InstructionExpr* p: \n";
+	VisitOutput *v = p->expr->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InstructionReturn* p)
 {
-    string val = "InstructionReturn* p: \n";
-    return new StringOutput(val);
+	string val = "InstructionReturn* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(InstructionStruct* p)
 {
-    string val = "InstructionStruct* p: \n";
-    return new StringOutput(val);
+	string val = "InstructionStruct* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(ElseBloc* p)
 {
-    string val = "ElseBloc* p: \n";
-    return new StringOutput(val);
+	string val = "ElseBloc* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(StructureControle* p)
 {
-    string val = "StructureControle* p: \n";
-    return new StringOutput(val);
+	string val = "StructureControle* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(StructureIf* p)
 {
-    string val = "StructureIf* p: \n";
-    return new StringOutput(val);
+	string val = "StructureIf* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(StructureWhile* p)
 {
-    string val = "StructureWhile* p: \n";
-    return new StringOutput(val);
+	string val = "StructureWhile* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(Bloc* p)
 {
-    string val = "Bloc* p: \n";
-    for (int i = 0; i < p->initDecl.size(); i++)
-    {
-        val += static_cast<StringOutput*>(p->initDecl[i]->accept(this))->val;
-    }
-    for (int i = 0; i < p->instructions.size(); i++)
-    {
-        val += static_cast<StringOutput*>(p->instructions[i]->accept(this))->val;
-    }
-    return new StringOutput(val);
+	string val = "Bloc* p: \n";
+	for (int i = 0; i < p->initDecl.size(); i++)
+	{
+		VisitOutput *v = p->initDecl[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
+	}
+	for (int i = 0; i < p->instructions.size(); i++)
+	{
+		VisitOutput *v = p->instructions[i]->accept(this);
+		val += static_cast<StringOutput*>(v)->val;
+		delete v;
+
+	}
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(BlocStruct* p)
 {
-    string val = "BlocStruct* p: \n";
-    return new StringOutput(val);
+	string val = "BlocStruct* p: \n";
+	return new StringOutput(val);
 }
 
 VisitOutput* MappingNameVisit::visit(LeftValue* p)
 {
-    string val = "LeftValue* p: \n";
-    val += static_cast<StringOutput*>(p->name->accept(this))->val;
-    return new StringOutput(val);
+	string val = "LeftValue* p: \n";
+	VisitOutput *v = p->name->accept(this);
+	val += static_cast<StringOutput*>(v)->val;
+	delete v;
+
+	return new StringOutput(val);
 }
